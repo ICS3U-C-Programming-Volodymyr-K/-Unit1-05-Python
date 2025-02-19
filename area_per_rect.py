2d0ffb7c837d85529907427b6b19a446c01f2e04
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-02-19 17:37:25 +0000

    Made area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,10 @@
+print("If the the length of one side if 5 cm and of another is cm, find the perimeter and the area area.")
+
+
+def main():
+    print("The perimeter is 2*(7+5)={}".format(2 * 7 + 2 * 5))
+    print(" The area is 7*5={}".format(7 * 5))
+
+
+if __name__ == "__main__":
+    main()
